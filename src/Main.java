dbccd288c9c96925cf56393484651406f8a36ad9
1E10 Laptop <sample@bths.edu>
2022-10-11 10:58:40 -0400

    .

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String turtle = "turtles are the best!";
+        String str1 = turtle.substring(16);
+        System.out.println(str1);
+        String str2 = turtle.substring(0,7);
+        System.out.println(str2);
+
+    }
+}
